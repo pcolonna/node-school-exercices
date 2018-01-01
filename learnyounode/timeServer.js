556f4217e213b04f04580fc946fdc07560858098
pierre <pierre.col.di@gmail.com>
2018-01-01 17:03:43 +0100

    Add timeserver problem

---
+++ learnyounode/timeServer.js
@@ -0,0 +1,53 @@
+/*
+  Exercise 10
+
+  Write a TCP time server!  
+   
+  Your server should listen to TCP connections on the port provided by the  
+  first argument to your program. For each connection you must write the  
+  current date & 24 hour time in the format:  
+   
+     "YYYY-MM-DD hh:mm"  
+   
+  followed by a newline character
+*/
+
+
+
+const net = require('net')
+const moment = require('moment')
+const port = process.argv[2]
+
+const server = net.createServer(function (socket) {
+ const data = moment().format("YYYY-MM-DD HH:mm")
+ socket.end(data + '\n')
+})
+
+server.listen(port)
+
+
+
+/* Official solution
+
+var net = require('net')
+    
+    function zeroFill (i) {
+      return (i < 10 ? '0' : '') + i
+    }
+    
+    function now () {
+      var d = new Date()
+      return d.getFullYear() + '-' +
+        zeroFill(d.getMonth() + 1) + '-' +
+        zeroFill(d.getDate()) + ' ' +
+        zeroFill(d.getHours()) + ':' +
+        zeroFill(d.getMinutes())
+    }
+    
+    var server = net.createServer(function (socket) {
+      socket.end(now() + '\n')
+    })
+    
+    server.listen(Number(process.argv[2]))
+
+*/
