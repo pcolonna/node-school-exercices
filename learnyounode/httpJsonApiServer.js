3450ca01a70da784fff010a9529acd88a0fab749
pierre <pierre.col.di@gmail.com>
2018-01-01 21:32:01 +0100

    Add httpJsonApiServer problem

---
+++ learnyounode/httpJsonApiServer.js
@@ -0,0 +1,81 @@
+/*
+  Exercise 13
+
+  Write a TCP time server!  
+   
+  Your server should listen to TCP connections on the port provided by the  
+  first argument to your program. For each connection you must write the  
+  current date & 24 hour time in the format:  
+   
+     "YYYY-MM-DD hh:mm"  
+   
+  followed by a newline character
+*/
+
+
+
+const http = require('http')
+const url = require('url')
+const port = process.argv[2]
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime: time.getTime() }
+}
+
+const server = http.createServer(function (req, res) {
+  const parsedUrl = url.parse(req.url, true)
+  const time = new Date(parsedUrl.query.iso)
+  let result
+
+  if (/^\/api\/parsetime/.test(req.url)) {
+    result = parsetime(time)
+  } else if (/^\/api\/unixtime/.test(req.url)) {
+    result = unixtime(time)
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(result))
+  } else {
+    res.writeHead(404)
+    res.end()
+  } 
+ 
+})
+
+server.listen(port)
+
+
+
+/* Official solution
+
+var net = require('net')
+    
+    function zeroFill (i) {
+      return (i < 10 ? '0' : '') + i
+    }
+    
+    function now () {
+      var d = new Date()
+      return d.getFullYear() + '-' +
+        zeroFill(d.getMonth() + 1) + '-' +
+        zeroFill(d.getDate()) + ' ' +
+        zeroFill(d.getHours()) + ':' +
+        zeroFill(d.getMinutes())
+    }
+    
+    var server = net.createServer(function (socket) {
+      socket.end(now() + '\n')
+    })
+    
+    server.listen(Number(process.argv[2]))
+
+*/
